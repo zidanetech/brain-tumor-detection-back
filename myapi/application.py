43297803bac790b9abfb3b9f9a726b23e6779153
unknown <chaabaniisraa@gmail.com>
2023-12-04 01:06:41 +0100

    add news code back

--- myapi/application.py
+++ myapi/application.py
@@ -5,43 +5,39 @@ from tensorflow import keras
 from keras.models import load_model
 from keras.preprocessing import image as kerasIMG
 from PIL import Image
-
-"""def processing(img_url):
-    images = []
-    req = urlopen(img_url)
-    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-    image = cv2.imdecode(arr, -1) # 'Load it as it is'
-
-    
-    img = cv2.imread(image)
-    img = Image.fromarray(img)
-    img = img.resize((224, 224))
-    img = np.array(img)
-    input_img = np.expand_dims(img, axis=0)
-    return input_img     
-"""
-from PIL import Image
- 
-def convert_tiff_to_png(input_path, output_path):
-    # Open the TIFF image
-    with Image.open(input_path) as img:
-        # Save the image in PNG format
-        img.save(output_path, 'PNG')
- 
-# Example usage
-convert_tiff_to_png('path/to/your/input_image.tif', 'path/to/your/output_image.png')
-
-
-
 #from tensorflow.keras.models import load_model
-model = load_model("C:/Users/IsRa.ChaaBani/Desktop/application_brain_tumor_detection/brain-tumor-detection-back/myapi/models/model_cnn_1.h5")
 
-def make_prediction(img):
+model = load_model("C:/Users/IsRa.ChaaBani/Desktop/application_brain_tumor_detection/brain-tumor-detection-back/myapi/models/best_model.h5")
+print('Model loaded. http://127.0.0.1:8000/')
+
+def get_className(classNo):
+    print(classNo)
+    if classNo==0:
+        return "No Brain Tumor"
+    elif classNo==1:
+        return "Yes Brain Tumor"
+    
+ 
+ 
+def getResult(img):
+    image=cv2.imread(img)
+    image = Image.fromarray(image, 'RGB')
+    image = image.resize((224, 224))
+    image=np.array(image)
+    input_img = np.expand_dims(image, axis=0)
+    result=model.predict(input_img)
+    return result
+    
+
+      
+
+
+"""def make_prediction(img):
     images = []
     req = urlopen(img)
     arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
     image = cv2.imdecode(arr, -1) # 'Load it as it is'
-    convert_tiff_to_png
+ 
     
     
     img=Image.fromarray(image)
@@ -54,12 +50,12 @@ def make_prediction(img):
         print("Tumor Detected")
     else:
         print("No Tumor ")
-
-
+"""
 """def model_loading(image):
     np_array = np.array(image)
     print(np_array.shape)
     model = load_model('C:/Users/IsRa.ChaaBani/Desktop/application_brain_tumor_detection/brain-tumor-detection-back/myapi/models/model_cnn_1.h5')
     output = model.predict(np_array)
     return output
-    """
+ """
+
--- myapi/views.py
+++ myapi/views.py
@@ -1,9 +1,15 @@
+import os
+from urllib.request import urlopen
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from . import application
 import cv2
 import random
+from werkzeug.utils import secure_filename
+import numpy as np
+
+
 
 # Create your views here.
 
@@ -21,6 +27,18 @@ def getData(request):
 # Prediction URL
 @api_view(['POST'])
 def make_prediction(request):
-    # print(request.data.get('img'))
-    image = application.make_prediction(request.data.get('img')) # get image's matrix
-    print(image)
+    f=request.data.get('img')
+    req = urlopen(f)
+    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    image = cv2.imdecode(arr, -1) # 'Load it as it is'
+    basepath = os.path.dirname(__file__)
+    """file_path = os.path.join(
+                basepath, 'image', secure_filename(image.filename))"""
+    #f.save(file_path)
+    cv2.imwrite("nom_fichier.tif", image)
+    value= application.getResult("nom_fichier.tif")
+    result= application.get_className(value)
+    print(result)
+    return Response({
+        "message": result
+    })
